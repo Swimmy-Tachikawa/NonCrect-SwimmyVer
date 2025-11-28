2b1a760eee48bc79262a7abb9bb1307c1654d13b
Swimmy-Tachikawa <swimmy_tachikawa001@sai.co.jp>
2025-11-28 16:29:08 +0900

    Remove src

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-import CodingTools2
-import NonCrect
-
-
-if __name__ == '__main__':
-    ...
--- tests/run.py
+++
@@ -1,8 +0,0 @@
-
-
-from NonCrect.Engine import Engine
-
-
-if __name__ == '__main__':
-    Engine().exe()
-    ...
